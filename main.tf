a0f3cce3e029b1d16f11632b3d5a5272486d5823
Oleg Stakhanov <oll.lol.123@gmail.com>
2026-05-10 20:31:43 +0300

    try to ignore upgrade_settings

--- main.tf
+++ main.tf
@@ -18,7 +18,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   lifecycle {
     ignore_changes = [
       default_node_pool[0].node_count,
-      default_node_pool[0].tags
+      default_node_pool[0].tags,
+      upgrade_settings
     ]
   }
 
